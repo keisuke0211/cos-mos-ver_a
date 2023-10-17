c8f397cbc476f0683ab4b2cab2dd1c620dfa9720
HiromuKomuro <221033@st.yoshida-g.ac.jp>
2023-10-17 10:11:28 +0900

    隕石の移動の追加

--- code/Project/Object/Gimmick/meteor.cpp
+++ code/Project/Object/Gimmick/meteor.cpp
@@ -12,7 +12,9 @@
 //==========| CTrampolineクラスのメンバ関数
 //----------|---------------------------------------------------------------------
 //================================================================================
-
+D3DXVECTOR3 static const METEOR_ADDROT = { 0.1f,0.2f,0.1f };	// 隕石の回転の移動量
+D3DXVECTOR3 static const METEOR_UNINIT_MINPOS = { -1000.0f,-1000.0f,0.0f };	// 最小の位置
+D3DXVECTOR3 static const METEOR_UNINIT_MAXPOS = { 1000.0f,1000.0f,0.0f };	// 最大の位置
 //========================================
 // コンストラクタ
 //========================================
@@ -20,6 +22,7 @@ CMeteor::CMeteor(void) {
 	Manager::BlockMgr()->AddList(this);
 
 	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 	m_move = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 }
 
@@ -54,8 +57,15 @@ void CMeteor::Update(void) {
 
 	// 移動量の追加
 	m_pos += m_move;
+	// 向きの移動量の追加
+	m_rot += METEOR_ADDROT;
 
-	RNLib::Model()->Put(m_pos, D3DXVECTOR3(0.0f, 0.0f, 0.0f), ModelIdx, false);
+	RNLib::Model()->Put(m_pos, m_rot, ModelIdx, false);
+
+	if (m_pos.x >= METEOR_UNINIT_MAXPOS.x || m_pos.y >= METEOR_UNINIT_MAXPOS.y || m_pos.x <= METEOR_UNINIT_MINPOS.x || m_pos.y <= METEOR_UNINIT_MINPOS.y)
+	{
+
+	}
 }
 
 //========================================
