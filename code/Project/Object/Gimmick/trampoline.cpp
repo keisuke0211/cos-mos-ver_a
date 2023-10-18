143ee2f5f5a27b217d1b87a1d83f5074cb37d16b
RyukiFuziwara <221069@st.yoshida-g.ac.jp>
2023-10-18 11:06:56 +0900

    トランポリン挙動追加

--- code/Project/Object/Gimmick/trampoline.cpp
+++ code/Project/Object/Gimmick/trampoline.cpp
@@ -8,6 +8,8 @@
 #include "../../main.h"
 #include "../../../_RNLib/_Basis/Other/input.h"
 
+#define MAX_COUNT (30)	//最大カウント数
+
 //================================================================================
 //----------|---------------------------------------------------------------------
 //==========| CTrampolineクラスのメンバ関数
@@ -27,11 +29,11 @@ CTrampoline::CTrampoline(void) {
 	m_state = STATE::NONE;
 	m_scale = D3DXVECTOR3(1.0f, 1.0f, 1.0f);
 	m_bLand = false;
-	m_modelIdx[0] = RNLib::Model()->Load("data\\MODEL\\Block.x");
-	m_modelIdx[1] = RNLib::Model()->Load("data\\MODEL\\SpringFloor.x");
-	m_modelIdx[2] = RNLib::Model()->Load("data\\MODEL\\SpringFloor.x");
+	m_modelIdx[0] = RNLib::Model()->Load("data\\MODEL\\Spring_Middle.x");
+	m_modelIdx[1] = RNLib::Model()->Load("data\\MODEL\\Spring_Up.x");
+	m_modelIdx[2] = RNLib::Model()->Load("data\\MODEL\\Spring_Down.x");
+	m_fJamp = 8.0f;
 	m_nCnt = 1;
-	m_fDifY = 0.0f;
 }
 
 //========================================
@@ -63,49 +65,46 @@ void CTrampoline::Uninit(void) {
 //========================================
 void CTrampoline::Update(void) {
 
-	//位置記憶
-	m_posOld = m_pos;
-
 	if (RNLib::Input()->KeyTrigger(DIK_UPARROW))
 	{
+		m_nCnt = MAX_COUNT;
 		m_state = STATE::UP_LAND;
-
-		m_fDifY = 20.0f;
-
-		m_nCnt = 30;
 	}
 	if (RNLib::Input()->KeyTrigger(DIK_DOWNARROW))
 	{
+		m_nCnt = MAX_COUNT;
 		m_state = STATE::DOWN_LAND;
-
-		m_fDifY = -20.0f;
-
-		m_nCnt = 30;
 	}
 
-	D3DXMATRIX mtx = ConvPosRotToMatrix(m_pos, D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-	D3DXMatrixScaling(&mtx, m_scale.x, m_scale.y, m_scale.z);
-	RNLib::Model()->Put(mtx, m_modelIdx[0], false);
+	RNLib::Model()->Put(m_pos, D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[0], false);
 
-	D3DXMATRIX mtx1 = ConvPosRotToMatrix(D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-	D3DXMatrixScaling(&mtx1, m_scale.x/3, m_scale.y/3, m_scale.z/3);
-	RNLib::Model()->Put(D3DXVECTOR3(0.0f, 10.0f + m_fDifY / m_nCnt, 0.0f), D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[1], false);
-
-	RNLib::Model()->Put(D3DXVECTOR3(m_pos.x, m_pos.y + 20.0f, m_pos.z), D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[1], false);
-	RNLib::Model()->Put(D3DXVECTOR3(m_pos.x, m_pos.y + -20.0f, m_pos.z), D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[2], false);
-
-	if (m_nCnt > 1 && m_state != STATE::NONE)
-	{
+	if (m_nCnt > 0) {
 		m_nCnt--;
+		if(m_nCnt == 0)
+			m_state = STATE::NONE;
 	}
-	else if(m_nCnt < 30)
-	{
-		m_state = STATE::NONE;
 
-		m_nCnt++;
+	if (m_state != STATE::NONE)
+	{
+		float fCountRate = Easing(EASE_IN, m_nCnt, MAX_COUNT);
+		
+		if (m_state == STATE::UP_LAND)
+		{
+			RNLib::Model()->Put(D3DXVECTOR3(m_pos.x, m_pos.y, m_pos.z), D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[1], false);
+			RNLib::Model()->Put(D3DXVECTOR3(m_pos.x, m_pos.y - m_fJamp * fCountRate, m_pos.z), D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[2], false);
+		}
+		else if (m_state == STATE::DOWN_LAND)
+		{
+			RNLib::Model()->Put(D3DXVECTOR3(m_pos.x, m_pos.y + m_fJamp * fCountRate, m_pos.z), D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[1], false);
+			RNLib::Model()->Put(D3DXVECTOR3(m_pos.x, m_pos.y, m_pos.z), D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[2], false);
+		}
+	}
+	else if (m_state == STATE::NONE)
+	{
+		RNLib::Model()->Put(m_pos, D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[1], false);
+		RNLib::Model()->Put(m_pos, D3DXVECTOR3(0.0f, 0.0f, 0.0f), m_modelIdx[2], false);
 	}
 }
-
 //========================================
 // 描画処理
 // Author:RYUKI FUJIWARA
